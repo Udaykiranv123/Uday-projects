5717264441440dfd611f9daa4b49143685bfe83b
Udaykiranv123 <vemulauday90@gmail.com>
2025-06-04 12:21:25 +0530

    Add files via upload

---
+++ LambdaExample.java
@@ -0,0 +1,14 @@
+
+package dsa;
+interface message{
+	void say();
+}
+public class LambdaExample {
+
+	public static void main(String[] args) {
+		message hellomessage = () ->System.out.println("This is lambda example");
+		hellomessage.say();
+
+	}
+
+}
